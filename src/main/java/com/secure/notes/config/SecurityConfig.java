950e174924615f2071db7c1792938945c699ed44
Md Irfan <irfan02md@gmail.com>
2024-11-01 17:08:40 +0530

    Added csrf protection

--- src/main/java/com/secure/notes/config/SecurityConfig.java
+++ src/main/java/com/secure/notes/config/SecurityConfig.java
@@ -1,17 +1,16 @@
 package com.secure.notes.config;
 
 import com.secure.notes.filters.CustomLoggingFilter;
-import com.secure.notes.filters.RequestValidationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -22,21 +21,18 @@ public class SecurityConfig {
     @Autowired
     private CustomLoggingFilter customLoggingFilter;
 
-    @Autowired
-    private RequestValidationFilter requestValidationFilter;
-
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
         httpSecurity.authorizeHttpRequests(request ->
                 request
                         .requestMatchers("/api/notes/admin/**").hasRole("ADMIN")
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
         );
-        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.formLogin(withDefaults());
         httpSecurity.httpBasic(withDefaults());
         httpSecurity.addFilterBefore(customLoggingFilter, UsernamePasswordAuthenticationFilter.class);
-        httpSecurity.addFilterAfter(requestValidationFilter, CustomLoggingFilter.class);
         return httpSecurity.build();
     }
 
